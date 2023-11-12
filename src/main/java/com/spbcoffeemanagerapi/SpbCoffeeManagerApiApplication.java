13246f83177c1f3e23489cf3b5b074d79ec8a6c5
lythanhtung-github <lythanhtung.work@gmail.com>
2023-11-12 21:10:40 +0700

    init

---
+++ src/main/java/com/spbcoffeemanagerapi/SpbCoffeeManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.spbcoffeemanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbCoffeeManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbCoffeeManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spbcoffeemanagerapi/SpbCoffeeManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spbcoffeemanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbCoffeeManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
